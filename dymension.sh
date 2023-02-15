11d714d641f5349f282818ef031a3eceab2cfe8d
ALRIZKI HAIKAL <115471452+sipalingtestnet@users.noreply.github.com>
2023-02-16 03:04:12 +0800

    Create dymension.sh

---
+++ dymension.sh
@@ -0,0 +1,132 @@
+#
+# // Copyright (C) 2022 Salman Wahib Recoded By NodeX Capital
+#
+
+#!/bin/bash
+clear
+
+echo -e "\033[0;33m"
+echo "================================================================="
+echo " ░██████╗██████╗░████████╗░░░░░░███╗░░██╗░█████╗░██████╗░███████╗"
+echo " ██╔════╝██╔══██╗╚══██╔══╝░░░░░░████╗░██║██╔══██╗██╔══██╗██╔════╝"
+echo " ╚█████╗░██████╔╝░░░██║░░░█████╗██╔██╗██║██║░░██║██║░░██║█████╗░░"
+echo " ░╚═══██╗██╔═══╝░░░░██║░░░╚════╝██║╚████║██║░░██║██║░░██║██╔══╝░░"
+echo " ██████╔╝██║░░░░░░░░██║░░░░░░░░░██║░╚███║╚█████╔╝██████╔╝███████╗"
+echo " ╚═════╝░╚═╝░░░░░░░░╚═╝░░░░░░░░░╚═╝░░╚══╝░╚════╝░╚═════╝░╚══════╝"
+echo "================================================================="
+echo -e "\e[0m"
+echo -e '\e[33mNama Project =\e[55m' Coreum 
+echo -e '\e[33mKomunitas Kami =\e[55m' Sipaling Testnet X CNESIA112
+echo -e '\e[33mChannel Telegram =\e[55m' https://t.me/ssipalingtestnet
+echo -e '\e[33mGroup Telegram =\e[55m' https://t.me/diskusisipalingairdrop
+echo -e "\e[0m"
+echo -e "\033[0;33m"
+echo "================================================================="
+
+
+sleep 1
+
+
+PORT=52
+
+
+echo "export PORT=${PORT}" >> $HOME/.bash_profile
+source $HOME/.bash_profile
+echo export CHAIN_ID="35-C" >> $HOME/.bash_profile
+source ~/.bash_profile
+# Set Vars
+if [ ! $NODENAME ]; then
+	read -p "[ENTER YOUR NODE] > " NODENAME
+	echo 'export NODENAME='$NODENAME >> $HOME/.bash_profile
+fi
+echo ""
+echo -e "YOUR NODE NAME : \e[1m\e[35m$NODENAME\e[0m"
+echo -e "NODE CHAIN CHAIN  : \e[1m\e[35mcoreum-testnet-1\e[0m"
+echo -e "NODE PORT      : \e[1m\e[35m$PORT\e[0m"
+echo ""
+
+
+# Package
+sudo apt update && sudo apt upgrade -y
+sudo apt install curl tar wget clang pkg-config libssl-dev libleveldb-dev jq build-essential bsdmainutils git make ncdu htop screen unzip bc fail2ban htop -y
+
+# Install GO
+ver="1.19" && \
+wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz" && \
+sudo rm -rf /usr/local/go && \
+sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz" && \
+rm "go$ver.linux-amd64.tar.gz" && \
+echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile && \
+source $HOME/.bash_profile && \
+go version
+
+
+#INSTALL
+cd $HOME
+git clone https://github.com/dymensionxyz/dymension.git --branch v0.2.0-beta
+cd dymension
+make install
+
+dymd init $Validator_Name --chain-id $CHAIN_ID
+
+wget https://raw.githubusercontent.com/obajay/nodes-Guides/main/Dymension/genesis.json -O $HOME/.dymension/config/genesis.json
+wget -O $HOME/.dymension/config/addrbook.json "https://raw.githubusercontent.com/obajay/nodes-Guides/main/Dymension/addrbook.json"
+
+
+#setpeer
+
+sed -i.bak -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0.0udym\"/;" ~/.dymension/config/app.toml
+sed -i -e "s/^filter_peers *=.*/filter_peers = \"true\"/" $HOME/.dymension/config/config.toml
+external_address=$(wget -qO- eth0.me) 
+sed -i.bak -e "s/^external_address *=.*/external_address = \"$external_address:26656\"/" $HOME/.dymension/config/config.toml
+peers="dc237ba44f4f178f6a72b60d9dee2337d424bfce@65.109.85.226:26656,3515bc6054d3e71caf2e04effaad8c95ee4b6dc6@165.232.186.173:26656,e9a375501c0a2eab296a16753667c708ed64649e@95.214.53.46:26656,2d05753b4f5ac3bcd824afd96ea268d9c32ed84d@65.108.132.239:26656"
+sed -i.bak -e "s/^persistent_peers *=.*/persistent_peers = \"$peers\"/" $HOME/.dymension/config/config.toml
+seeds=""
+sed -i.bak -e "s/^seeds =.*/seeds = \"$seeds\"/" $HOME/.dymension/config/config.toml
+sed -i 's/max_num_inbound_peers =.*/max_num_inbound_peers = 50/g' $HOME/.dymension/config/config.toml
+sed -i 's/max_num_outbound_peers =.*/max_num_outbound_peers = 50/g' $HOME/.dymension/config/config.toml
+
+
+
+
+# pruning and indexer
+pruning="custom"
+pruning_keep_recent="100"
+pruning_keep_every="0"
+pruning_interval="10"
+sed -i -e "s/^pruning *=.*/pruning = \"$pruning\"/" $HOME/.dymension/config/app.toml
+sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"$pruning_keep_recent\"/" $HOME/.dymension/config/app.toml
+sed -i -e "s/^pruning-keep-every *=.*/pruning-keep-every = \"$pruning_keep_every\"/" $HOME/.dymension/config/app.toml
+sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"$pruning_interval\"/" $HOME/.dymension/config/app.toml
+indexer="null" && \
+sed -i -e "s/^indexer *=.*/indexer = \"$indexer\"/" $HOME/.dymension/config/config.toml
+
+
+sudo tee /etc/systemd/system/dymd.service > /dev/null <<EOF
+[Unit]
+Description=dymd
+After=network-online.target
+
+[Service]
+User=$USER
+ExecStart=$(which dymd) start
+Restart=on-failure
+RestartSec=3
+LimitNOFILE=65535
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+
+# start service
+sudo systemctl daemon-reload
+sudo systemctl enable dymd
+sudo systemctl restart dymd
+
+
+echo -e "\033[0;33m"
+echo -e "\e[1m\e[33 DONE MASBRO\e[0m"
+echo -e "CHECK RUNNING LOGS : \e[1m\e[33msudo journalctl -u dymd -f -o cat\e[0m"
+echo ""
+
